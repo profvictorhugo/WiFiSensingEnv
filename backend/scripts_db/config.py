4cd30281566a27686c0ddb143244b7ffbb8f25c0
MrCode-Man <minimina722@gmail.com>
2025-06-23 13:33:12 -0300

    craido a pasta de separação entre scripts para SQL

---
+++ backend/scripts_db/config.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+import os
+
+app = Flask(__name__)
+CORS(app)
+
+# tudo devidamente configurado
+app.config['SECRET_KEY'] = os.urandom(24) # não temos chaves, toma uma aleatória
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost:3306/wifisensing'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
