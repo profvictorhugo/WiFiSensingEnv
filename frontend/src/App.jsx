e0be73d613ed9fe73c5f76c281d12c4bd86f5aa0
Ana Luiza <analuizamsantana@gmail.com>
2026-04-24 00:21:04 -0300

    feat: telas ja existentes no projeto ajustadas e adicionadas pagina principal e usuarios

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,27 @@
 import Login from "./pages/Login/Login.jsx";
+import Home from "./pages/Home/Home.jsx";
+import Usuarios from "./pages/Usuarios/Usuarios.jsx";
+import Datasets from "./pages/Datasets/Datasets.jsx";
+import DatasetDetalhe from "./pages/DatasetDetalhe/DatasetDetalhe.jsx";
+import Modelos from "./pages/Modelos/Modelos.jsx";
+import Configuracoes from "./pages/Configuracoes/Configuracoes.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/usuarios" element={<Usuarios />} />
+          <Route path="/datasets" element={<Datasets />} />
+          <Route path="/datasets/:datasetId" element={<DatasetDetalhe />} />
+          <Route path="/modelos" element={<Modelos />} />
+          <Route path="/modelos/:modeloId" element={<Modelos />} />
+          <Route path="/configuracoes" element={<Configuracoes />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
